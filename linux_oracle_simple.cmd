97622c52b9c650746cc2355f205537d71d9969d3
khatn <khatn2705@gmail.com>
2014-06-06 11:50:27 +0700

    Update linux_oracle_simple.cmd

--- linux_oracle_simple.cmd
+++ linux_oracle_simple.cmd
@@ -4,6 +4,9 @@ top –c : dùng để xem thông tin hoạt động của server
 free –m : dùng để kiểm tra thông tin bộ nhớ
 df –h : dùng để kiểm tra dụng lượng ổ cứng
 sync; echo 3 > /proc/sys/vm/drop_caches : dùng để giải phóng caches và làm tăng bộ nhớ
+zip -r tenthumuc.zip tenthumuc :  nén thư mục 
+unzip tenthumuc.zip
+
 
 Oracle
 ===========
@@ -26,3 +29,9 @@ alter database datafile '/u01/oradata/qlcb/users01.dbf' autoextend on next 5m ma
 ---import & export---
 expdp HSCV_BTP_FINAL/HSCV_BTP_FINAL schemas=HSCV_BTP_FINAL directory=DMPDIR dumpfile=file.dmp logfile=export.log
 impdp HSCV_NEW/HSCV_NEW  REMAP_SCHEMA=HSCV_FINAL:HSCV_NEW directory=DMPDIR dumpfile=file.dmp logfile=import.log
+
+-- Xoa limit password, doi password dinh ky, so lan login sai
+SELECT profile FROM dba_users where username = 'QLQT';
+ALTER PROFILE DEFAULT LIMIT               //DEFAULT là tên profile cần đổi, ở đây profile mặc định của user 'QLQT' là DEFAULT
+FAILED_LOGIN_ATTEMPTS UNLIMITED
+PASSWORD_LIFE_TIME UNLIMITED;
