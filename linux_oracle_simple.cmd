9ccdad570fd4a690cbf91e6a25b2ae0874fc6683
khatn <khatn2705@gmail.com>
2014-09-10 10:06:59 +0700

    Update linux_oracle_simple.cmd

--- linux_oracle_simple.cmd
+++ linux_oracle_simple.cmd
@@ -9,6 +9,7 @@ unzip tenthumuc.zip
 netstat -an |grep :80 |wc -l: xem có bao nhiêu kết nối đến cổng 80
 mkdir: tạo thư mục mới
 rm -rf tenthumuc/tenfile: xóa tệp tin
+chmod -R 0777 /tênthưmục : phân quyền đọc ghi file trên folder cho tất cả user
 CATALINA_OPTS="-Xmx1g -XX:PermSize=256M -XX:MaxPermSize=500m" -- Tăng bộ nhớ Tomcat khi bị PermGem, viết trong Catalina.sh(Linux) hoặc Catalina.bat(Window)
 
 Oracle
